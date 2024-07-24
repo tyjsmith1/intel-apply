73234bab2613635e63a971e1e0f1305b79220803
tylersmith <tyjsmith1@gmail.com>
2024-07-24 16:45:23 -0500

    styled hero

--- intel-apply/src/components/name this later/FlipWordsDemo.jsx
+++ intel-apply/src/components/name this later/FlipWordsDemo.jsx
@@ -1,14 +1,27 @@
 import { FlipWords } from "../ui/flip-words";
 
 export function FlipWordsDemo() {
-  const words = ["hired", "strategic", "noticed", "recognized", "ahead", "prepared", "valued", "discovered", "insightful"];
+  const words = [
+    "hired",
+    "strategic",
+    "noticed",
+    "recognized",
+    "ahead",
+    "prepared",
+    "valued",
+    "discovered",
+    "insightful"
+  ];
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
       <div className="text-6xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        Get
-        <FlipWords words={words} /> <br />
-        with Hire Ground
+        <div>
+          Get <span className="inline-block"><FlipWords words={words} /></span>
+        </div>
+        <div>
+          with <span className="gradient-text">Hire Ground</span>
+        </div>
       </div>
     </div>
   );
