e6a64459c90cd3142c0f5e7076c192cd3ffaac4a
tylersmith <tyjsmith1@gmail.com>
2024-07-24 08:24:54 -0500

    switch to Clerk Auth

--- intel-apply/server/index.js
+++ intel-apply/server/index.js
@@ -11,8 +11,15 @@ app.use(express.json());
 
 app.use('/jobs', jobRoutes);
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(5001, () => {
+    console.log('Server is running on port 5001');
+}).on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+        console.error('Port 5001 is already in use. Please use a different port.');
+        process.exit(1);
+    } else {
+        throw err;
+    }
 });
 
 module.exports = prisma;
---
+++ intel-apply/src/app/page.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+
+    return (
+        <div>
+            <h1>Intel Apply</h1>
+        </div>
+    );
+}
+
+export default Home;
